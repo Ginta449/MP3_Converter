07c2e9d4a7cfedf041137b016796a330298d3e70
Ginta Kivite <D00121871@student.dkit.ie>
2022-10-06 14:35:53 +0100

    Master: initial commit. Base

---
+++ MediaManager.cs
@@ -0,0 +1,44 @@
+﻿using MediaToolkit;
+using MediaToolkit.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoLibrary;
+
+namespace MP3_Converter
+{
+    public class MediaManager
+    {
+        public MediaManager(string folderPath, string videoLink) => Convert(folderPath, videoLink);
+
+        private void Convert(string folderPath, string videoLink)
+        {
+            var video = GetVideoLink(videoLink);
+            ConvertBytes(folderPath, video);
+            var inputFile = InputFile(folderPath, video);
+            var outputFile = OutputFile(folderPath, video);
+            ConvertVideo(inputFile, outputFile);
+        }
+
+        private YouTubeVideo GetVideoLink(string vidoeLink) => YouTube.Default.GetVideo(vidoeLink);
+
+        private void ConvertBytes(string folderPath, YouTubeVideo videoLink) => File.WriteAllBytes($"{folderPath}/{videoLink.FullName}", videoLink.GetBytes());
+
+        private MediaFile InputFile(string folderPath, YouTubeVideo videoLink) => new MediaFile { Filename = $"{folderPath}/{videoLink.FullName}" };
+
+        private MediaFile OutputFile(string folderPath, YouTubeVideo videoLink) => new MediaFile { Filename = $"{folderPath}/{videoLink.FullName}.mp3" };
+
+        private void ConvertVideo(MediaFile inputFile, MediaFile outputFile)
+        {
+            using (var engine = new Engine())
+            {
+                engine.GetMetadata(inputFile);
+
+                engine.Convert(inputFile, outputFile);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,20 @@ namespace MP3_Converter
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter folder path or type stop to exit:");
+            string folderPath = Console.ReadLine();
+
+            for(int i=0; i<100000; i++)
+            {
+                if(!folderPath.Contains("stop".ToUpper()))
+                {
+                    Console.WriteLine("Enter video link:");
+                    string videoLink = Console.ReadLine();
+
+                    new MediaManager(folderPath, videoLink);
+                }
+            }
+            
         }
     }
 }
